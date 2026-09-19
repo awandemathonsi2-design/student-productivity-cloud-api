70699a77cefd7b93e8e57cca01a8936be1359d6c
Awande <awande.mathonsi2@gmail.com>
2026-09-19 16:46:47 +0200

    Feat: initialise Javalin API server

--- src/main/java/com/studentproductivity/Main.java
+++ src/main/java/com/studentproductivity/Main.java
@@ -1,4 +1,15 @@
 package com.studentproductivity;
 
+import io.javalin.Javalin;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create()
+                .get("/tasks", ctx -> ctx.json("Task endpoint is working"))
+                .start(7070);
+
+        System.out.println("Student Productivity API started on port 7070");
+    }
 }
